51efb014e77c3980d0fc4b0101b6a8f69205000b
NazarethV <nazarethvillar38@gmail.com>
2024-09-03 13:39:00 -0300

    'addProduct' in ProductServiceImpl

--- src/main/java/com/controlstock/entities/Product.java
+++ src/main/java/com/controlstock/entities/Product.java
@@ -49,7 +49,7 @@ public class Product {
     private String supplier;
 
 
-    private String imageUrl;
+    private String image;
 }
 
 
--- src/main/java/com/controlstock/service/ProductServiceImpl.java
+++ src/main/java/com/controlstock/service/ProductServiceImpl.java
@@ -2,12 +2,16 @@ package com.controlstock.service;
 
 
 import com.controlstock.dto.ProductDto;
+import com.controlstock.entities.Product;
 import com.controlstock.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
@@ -15,10 +19,12 @@ public class ProductServiceImpl implements ProductService{
 
     //Para la Base de datos
     private final ProductRepository productRepository;
+    private final FileService fileService;
 
-    //
-    public ProductServiceImpl(ProductRepository productRepository) {
+    //Parámetros del constructor
+    public ProductServiceImpl(ProductRepository productRepository, FileService fileService) {
         this.productRepository = productRepository;
+        this.fileService = fileService;
     }
 
     @Value("${project.imageProduct}")
@@ -28,9 +34,50 @@ public class ProductServiceImpl implements ProductService{
     private String baseUrl;
 
     @Override
-    public ProductDto addProduct(ProductDto movieDto, MultipartFile file) throws IOException {
-        return null;
+    public ProductDto addProduct(ProductDto productDto, MultipartFile file) throws IOException {
+        //1- Cargar archivo (verifico si el nombre del archivo ya existe o no)
+        if (Files.exists(Paths.get(path + File.separator + file.getOriginalFilename()))){
+            throw new RuntimeException("File already exists: Please enter another file name!")
+        }
+
+        String uploadedFileName = fileService.uploadFile(path, file); //Para cargar el archivo hay que traer el método de servicio de archivos 'FileService'
+
+        //2- Pongo cómo nombre de archivo el valor del campo 'imageProduct'
+        productDto.setImage(uploadedFileName);//Pongo el nombre al archivo
+
+        //3-Mapeo el producto DTO al objeto PRODUCT (ProductRepository guarda los datos en la DB y acepta objetos 'Product' por lo que es necesario mapearlo previamente)
+        //Asigno el Obj DTO al Obj Product
+        Product product = new Product(
+                null, //Para no poder de manera directa el ID
+                productDto.getName(),
+                productDto.getDescription(),
+                productDto.getPrice(),
+                productDto.getStock(),
+                productDto.getCategory(),
+                productDto.getSupplier(),
+                productDto.getImage()
+        );
+
+        //4- Guardo el nuevo Obj Product en la DB
+        Product savedProduct = productRepository.save(product);
+
+        //5- Genero la URL para la imagen/archivo del producto
+        String imageUrl = baseUrl + "/file/" + uploadedFileName;
+
+        //6- Genero la respuesta que va a devolver este método (Mapeo lo del Obj Product al Obj DTO)
+        return new ProductDto(
+                savedProduct.getProductId(),
+                savedProduct.getName(),
+                savedProduct.getDescription(),
+                savedProduct.getPrice(),
+                savedProduct.getStock(),
+                savedProduct.getCategory(),
+                savedProduct.getSupplier(),
+                savedProduct.getImage(),
+                imageUrl
+        );
     }
+
 
     @Override
     public ProductDto getProduct(Integer productId) {
