18bd453187f8b10e9bba3f7b0c576f6d84ae5069
NazarethV <nazarethvillar38@gmail.com>
2024-09-10 13:22:34 -0300

    Entity: RefreshToken

--- src/main/java/com/controlstock/auth/entities/RefreshToken.java
+++ src/main/java/com/controlstock/auth/entities/RefreshToken.java
@@ -1,4 +1,32 @@
 package com.controlstock.auth.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer tokenId;
+
+    @Column
+    @NotBlank(message = "Please enter refresh token value!")
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Instant expirationTime;
+
+    @OneToOne
+    private User user;
 }
