8713d482700be7a438114076eda22d9462a19232
NazarethV <nazarethvillar38@gmail.com>
2024-09-11 13:10:31 -0300

    Service: 'register' in AuthService

--- src/main/java/com/controlstock/auth/services/AuthService.java
+++ src/main/java/com/controlstock/auth/services/AuthService.java
@@ -1,7 +1,10 @@
 package com.controlstock.auth.services;
 
+import com.controlstock.auth.entities.User;
+import com.controlstock.auth.entities.UserRole;
 import com.controlstock.auth.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +15,27 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JwtService jwtService;
-    private final RefreshTokenS
+    private final RefreshTokenService refreshTokenService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(UserRole.USER)
+                .build();
+
+        User savedUser = userRepository.save(user);
+        var accessToken = jwtService.generateToken(savedUser);
+        var refreshToken = refreshTokenService.createRefreshToken(savedUser.getEmail());
+
+        return AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken.getRefreshToken())
+                .build();
+    }
+
+
 }
