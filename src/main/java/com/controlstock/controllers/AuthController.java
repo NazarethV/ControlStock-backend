6566cbc0e0b39554fcd1260fdcd44a7d65657b99
NazarethV <nazarethvillar38@gmail.com>
2024-09-17 16:02:00 -0300

    Controller: AuthController

--- src/main/java/com/controlstock/controllers/AuthController.java
+++ src/main/java/com/controlstock/controllers/AuthController.java
@@ -1,4 +1,68 @@
 package com.controlstock.controllers;
 
+import com.controlstock.auth.entities.RefreshToken;
+import com.controlstock.auth.entities.User;
+import com.controlstock.auth.services.AuthService;
+import com.controlstock.auth.services.JwtService;
+import com.controlstock.auth.services.RefreshTokenService;
+import com.controlstock.auth.utils.AuthResponse;
+import com.controlstock.auth.utils.LoginRequest;
+import com.controlstock.auth.utils.RefreshTokenRequest;
+import com.controlstock.auth.utils.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtService jwtService;
+
+
+    public AuthController(AuthService authService, RefreshTokenService refreshTokenService, JwtService jwtService) {
+        this.authService = authService;
+        this.refreshTokenService = refreshTokenService;
+        this.jwtService = jwtService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        RefreshToken refreshToken = refreshTokenService.verifyRefreshToken(refreshTokenRequest.getRefreshToken());
+        User user = refreshToken.getUser();
+
+        String accessToken = jwtService.generateToken(user);
+
+        return ResponseEntity.ok(AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken.getRefreshToken())
+                .build());
+    }
+
 }
+
+
+
+
+
+
+
+
+
