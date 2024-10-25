e5aaa3913ceab2f8cf49c189ef3f9a16317a07b2
NazarethV <nazarethvillar38@gmail.com>
2024-10-25 13:12:24 -0300

    fix: minor general improvements and bug fixes

--- src/main/java/com/controlstock/auth/config/SecurityConfiguration.java
+++ src/main/java/com/controlstock/auth/config/SecurityConfiguration.java
@@ -112,7 +112,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); //http://localhost:5173 -  Cambia por tus dominios permitidos
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); //"*"// -  Cambia por tus dominios permitidos
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
